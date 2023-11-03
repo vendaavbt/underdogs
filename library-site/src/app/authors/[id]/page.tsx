500c5f1e23b967821ffc5ba7e3681140fbec7452
Matteo <matteo.guignet@student.junia.com>
2023-11-03 14:37:06 +0100

    ListeAuteurs

--- library-site/src/app/authors/[id]/page.tsx
+++ library-site/src/app/authors/[id]/page.tsx
@@ -1,21 +1,49 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-import { FC } from 'react';
-import Menu from  'src/app/page';
+import React, { FC, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Menu from 'src/app/page';
 
 const AuthorDetailsPage: FC = () => {
-  const { id } = useParams();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [authorDetails, setAuthorDetails] = useState<AuthorDetails | null>(null);
+
+  const fetchAuthorDetails = async (authorId: string) => {
+    try {
+      // A remplacer par une vraie requête à l'API
+      const response = await fetch(`/api/authors/${authorId}`);
+      if (response.ok) {
+        const authorData = await response.json();
+        setAuthorDetails(authorData);
+      } else {
+        console.error('Erreur lors de la récupération des détails de l\'auteur');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des détails de l\'auteur', error);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchAuthorDetails(id as string);
+    }
+  }, [id]);
 
   return (
     <>
-      <Menu /> 
+      <Menu />
       <div className="text-center">
-        <p className="text-xl font-semibold">Page des auteurs</p>
+        <p className="text-xl font-semibold">Détails de l'auteur</p>
       </div>
-      Détails de l'auteur &apos;
-      {id}
-      &apos; ne sont pas implémentés
+      {authorDetails ? (
+        <div>
+          <p>ID: {authorDetails.id}</p>
+          <p>Nom: {authorDetails.firstName}</p>
+          <p>Prénom: {authorDetails.lastName}</p>
+        </div>
+      ) : (
+        <p>Détails de l'auteur &apos;{id}&apos; ne sont pas encore disponibles.</p>
+      )}
     </>
   );
 };
--- library-site/src/app/authors/page.tsx
+++ library-site/src/app/authors/page.tsx
@@ -2,14 +2,71 @@
 
 
 import Menu from  'src/app/page';
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 
-const AuthorsPage: FC = () => <>
-    <Menu />
-    <div className="text-center">
-        <p className="text-xl font-semibold">Page des auteurs</p>
+import { PlainAuthorModel } from 'library-api/src/entities'; 
+
+const AuthorPage: FC = () => {
+
+  const [authors, setAuthors] = useState<PlainAuthorModel[]>([
+    {
+      id: '1',
+      firstName: 'Auteur 1',
+      lastName: 'Nom 1',
+    },
+    {
+      id: '2',
+      firstName: 'Auteur 2',
+      lastName: 'Nom 2',
+    },
+
+  ]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [bookCount, setBookCount] = useState(0);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleBookCountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBookCount(parseInt(e.target.value, 10));
+  };
+
+  const filteredAuthors = authors.filter((author) => {
+    const fullName = `${author.firstName} ${author.lastName}`;
+    return (
+      fullName.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (bookCount === 0 || author.booksWritten >= bookCount)
+    );
+  });
+
+  return (
+    <div>
+      <h1>Liste des Auteurs</h1>
+      <input
+        type="text"
+        placeholder="Rechercher par nom"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <input
+        type="number"
+        placeholder="Rechercher par nombre de livres écrits"
+        value={bookCount}
+        onChange={handleBookCountChange}
+      />
+      <div>
+        {filteredAuthors.map((author) => (
+          <div key={author.id} className="auteur-card">
+            <img src={author.photoId} alt={`${author.firstName} ${author.lastName}`} />
+            <p>Nom: {`${author.firstName} ${author.lastName}`}</p>
+            <p>Nombre de livres écrits: {author.booksWritten}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    La page des auteurs n'est pas (encore) implémentée
-    </>;
+  );
+};
 
-export default AuthorsPage;
+export default AuthorPage;
